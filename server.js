c6a64df0537e329cc0252d45d343bfc77f6f41a1
Enid-Sky <enid.sky@protonmail.com>
2020-05-06 17:54:40 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,81 @@
+//Express to run server and routes
+const express = require('express');
+
+//Start up an instance of app
+const app = express();
+
+//Dependencies
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+
+//Cross origin allowance
+app.require('cors');
+app.use(corse());
+
+//Initialize the main project folder
+app.use(express.static('WebsiteTest'));
+
+
+const port = 8000;
+//spin up the server
+const server = app.listener(port, listening);
+//const server = app.listen(port,()=> {console.log(`running on localhost: ${port}`)});
+
+//Callback
+function listening() {
+  console.log('server is running');
+  console.log(`running on localhost: ${port}`);
+};
+
+
+
+/*
+*
+*
+----ES6 version----
+*
+*
+*
+//Express to run server and routes
+import express, {
+  static
+} from 'express';
+
+//Start up an instance of app
+const app = express();
+
+//Dependencies
+
+import {
+  urlencoded,
+  json
+} from 'body-parser';
+app.use(urlencoded({
+  extended: false
+}));
+app.use(json());
+
+//Cross origin allowance
+app.require('cors');
+app.use(corse());
+
+//Initialize the main project folder
+app.use(static('WebsiteTest'));
+
+
+const port = 8000;
+//spin up the server
+const server = app.listener(port, listening);
+//const server = app.listen(port,()=> {console.log(`running on localhost: ${port}`)});
+
+//Callback
+function listening() {
+  console.log('server is running');
+  console.log(`running on localhost: ${port}`);
+};
+
+*/
